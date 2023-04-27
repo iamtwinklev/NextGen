1cd7e17209742079f3a3255c46a6fab008f5174a
iamtwinklev <109031944+iamtwinklev@users.noreply.github.com>
2023-04-27 18:20:48 +0530

    Task 1

--- App.js
+++ App.js
@@ -1,28 +1,33 @@
 import './App.css';
 import FileUpload from './components/FileUpload';
-import Try from './components/TodoForm';
 import New from './components/New';
 import Normalmain from './components/Normalmain';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TodoApp from './components/TodoApp';
+// import MainForm from './components/MainForm';
+// import Final from './components/Final';
+import Mainform1 from './components/MainForm1';
+// import DynamicDropdown from './components/DynamicDropdown';
 
 function App() {
-  
-    return (
-      <div className="App">
-        
-        <New />
-        <BrowserRouter>
-          <Routes>
+
+  return (
+    <div className="App">
+
+      <New />
+      <BrowserRouter>
+        <Routes>
           <Route path="/file" element={<FileUpload />} />
           <Route path="/try" element={<TodoApp />} />
           <Route path="/normal" element={<Normalmain />} />
-          </Routes>
-        </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+      <Mainform1 />
+      {/* <Final/> */}
+      {/* <DynamicDropdown/> */}
 
-        
-      </div>
-    );
-  }
+    </div>
+  );
+}
 
-  export default App;
+export default App;
