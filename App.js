4926578af467f3e10b05cd8c00aae5eae9e07c05
iamtwinklev <109031944+iamtwinklev@users.noreply.github.com>
2023-04-27 18:23:54 +0530

    Task 1

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import FileUpload from './components/FileUpload';
-import New from './components/New';
+// import New from './components/New';
 import Normalmain from './components/Normalmain';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TodoApp from './components/TodoApp';
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
 
-      <New />
+      {/* <New /> */}
       <BrowserRouter>
         <Routes>
           <Route path="/file" element={<FileUpload />} />
